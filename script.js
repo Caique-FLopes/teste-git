98bee8edf20dfa180e3b38fdc44ef52bda92a8cd
JVDoniak <jvdoniak@gmail.com>
2026-04-07 09:33:02 -0400

    Resolução do exercicio14

--- script.js
+++ script.js
@@ -87,6 +87,22 @@
 /**
  * @author João Victor Doniak
  */
+function exercio8(){
+    function gerarTabuada(){
+        const entrada = prompt("Digite um número para ver a tabuada:");
+        const numero = parseFloat(entrada);
+        if (isNaN(numero)) {
+            console.error("Erro: Por favor, digite um número válido.");
+            return;
+        }
+
+        console.log('--- Tabuada do ${numero} ---');
+        for (let i = 1; i <= 10; i++) {
+            console.log('${numero} x ${i} = ${numero * i}');
+        }
+    }
+    gerarTabuada();
+}
 
 // 9 – Verificador de senha forte
 // Peça uma senha
@@ -97,7 +113,9 @@
 // Use função
 // Retorne mensagens diferentes
 // Trate entrada inválida
-/**
+
+
+/*
  * @author Larissa
  */
 
